baf45b1e26887042ed7505e81eae1b83f3e1ad38
alvarfs <146431698+alvarfs@users.noreply.github.com>
2024-02-04 12:27:39 +0100

    ex ended
    ex ended

--- Program.cs
+++ Program.cs
@@ -13,35 +13,36 @@
         // Codifica un programa que calcule el total de localidades vendidas (1) y el total de
         // localidades disponibles (0)
 
-        // int totalVendidas = 0;
-        // int totalDisponibles = 0;
+        int totalVendidas = 0;
+        int totalDisponibles = 0;
 
-        // int[][] rows = {
-        //     new int[] {1, 1, 1},
-        //     new int[] {1, 0, 1, 1},
-        //     new int[] {0, 0, 1, 1, 1},
-        //     new int[] {1, 1, 1, 1, 1},
-        //     new int[] {0, 1, 0, 1, 1}
-        // };
+        int[][] rows = {
+            new int[] {1, 1, 1},
+            new int[] {1, 0, 1, 1},
+            new int[] {0, 0, 1, 1, 1},
+            new int[] {1, 1, 1, 1, 1},
+            new int[] {0, 1, 0, 1, 1}
+        };
 
-        // foreach (int[] row in rows)
-        // {
-        //     foreach (int slot in row)
-        //     {
-        //         if (slot == 1)
-        //         {
-        //             totalVendidas += 1;
-        //         }
-        //         else
-        //         {
-        //             totalDisponibles += 1;
-        //         }
+        foreach (int[] row in rows)
+        {
+            foreach (int slot in row)
+            {
+                if (slot == 1)
+                {
+                    totalVendidas += 1;
+                }
+                else
+                {
+                    totalDisponibles += 1;
+                }
                 
-        //     }
-        // }
+            }
+        }
 
-        // Console.WriteLine($"total vendidas: {totalVendidas}");
-        // Console.WriteLine($"total disponibles: {totalDisponibles}");
+        Console.WriteLine($"total vendidas: {totalVendidas}");
+        Console.WriteLine($"total disponibles: {totalDisponibles}");
+        System.Console.WriteLine();
 
 
         // 2. El software de un hotel tiene un sistema de reservas con una matriz:
@@ -63,107 +64,108 @@
         //     - Reservar habitaciones (únicamente se pueden reservar las libres (O))
         //     - Modificar o borrar reserva.
 
-        // int userSelect, userFloor, userDoor;
+        int userSelect, userFloor, userDoor;
         
-        // do
-        // {
-        //     System.Console.WriteLine("Escribe un numero para elegir:");
-        //     System.Console.WriteLine("1: Mostrar habitaciones disponibles");
-        //     System.Console.WriteLine("2: Reservar habitaciones");
-        //     System.Console.WriteLine("3: Modificar o borrar reserva");
-        //     System.Console.WriteLine("4: Cerrar programa");
-        //     System.Console.WriteLine();
-        //     System.Console.Write("Tu eleccion: ");
-        //     userSelect = Convert.ToInt32(Console.ReadLine());
+        do
+        {
+            System.Console.WriteLine("Escribe un numero para elegir:");
+            System.Console.WriteLine("1: Mostrar habitaciones disponibles");
+            System.Console.WriteLine("2: Reservar habitaciones");
+            System.Console.WriteLine("3: Modificar o borrar reserva");
+            System.Console.WriteLine("4: Cerrar programa");
+            System.Console.WriteLine();
+            System.Console.Write("Tu eleccion: ");
+            userSelect = Convert.ToInt32(Console.ReadLine());
 
-        //     switch (userSelect)
-        //     {
-        //         case 1:
-        //             System.Console.WriteLine("HABITACIONES DISPONIBLES:");
-        //                 for (int i = 0; i < hotel.Length; i++)
-        //                 {
-        //                     for (int j = 0; j < hotel[i].Length; j++)
-        //                     {
-        //                         if (hotel[i][j] == "O")
-        //                         {
-        //                             Console.WriteLine($"Piso {i} | Puerta {j}");
-        //                         }
-        //                     }
-        //                 }
-        //             System.Console.WriteLine();
-        //             System.Console.WriteLine("Pulsa enter para continuar...");
-        //             Console.ReadLine();
-        //             break;
-        //         case 2:
-        //             System.Console.WriteLine("Que habitacion quieres reservar:");
-        //             System.Console.Write("Piso: ");
-        //             userFloor = Convert.ToInt32(Console.ReadLine());
-        //             System.Console.Write("Puerta: ");
-        //             userDoor = Convert.ToInt32(Console.ReadLine());
+            switch (userSelect)
+            {
+                case 1:
+                    System.Console.WriteLine("HABITACIONES DISPONIBLES:");
+                        for (int i = 0; i < hotel.Length; i++)
+                        {
+                            for (int j = 0; j < hotel[i].Length; j++)
+                            {
+                                if (hotel[i][j] == "O")
+                                {
+                                    Console.WriteLine($"Piso {i} | Puerta {j}");
+                                }
+                            }
+                        }
+                    System.Console.WriteLine();
+                    System.Console.WriteLine("Pulsa enter para continuar...");
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    System.Console.WriteLine("Que habitacion quieres reservar:");
+                    System.Console.Write("Piso: ");
+                    userFloor = Convert.ToInt32(Console.ReadLine());
+                    System.Console.Write("Puerta: ");
+                    userDoor = Convert.ToInt32(Console.ReadLine());
 
-        //             for (int i = 0; i < hotel.Length; i++)
-        //             {
-        //                 for (int j = 0; j < hotel[i].Length; j++)
-        //                 {
-        //                     if (i == userFloor && j == userDoor)
-        //                     {
-        //                         if (hotel[i][j] == "O")
-        //                         {
-        //                             hotel[i][j] = "X";
-        //                             Console.WriteLine($"Piso {i} | Puerta {j} RESERVADA!");
-        //                         }
-        //                         else
-        //                         {
-        //                             Console.WriteLine($"Piso {i} | Puerta {j} ESTA OCUPADA!");
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //             System.Console.WriteLine();
-        //             System.Console.WriteLine("Pulsa enter para continuar...");
-        //             Console.ReadLine();
-        //             break;
-        //         case 3:
-        //             System.Console.WriteLine("Que reserva quieres borrar:");
-        //             System.Console.Write("Piso: ");
-        //             userFloor = Convert.ToInt32(Console.ReadLine());
-        //             System.Console.Write("Puerta: ");
-        //             userDoor = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 0; i < hotel.Length; i++)
+                    {
+                        for (int j = 0; j < hotel[i].Length; j++)
+                        {
+                            if (i == userFloor && j == userDoor)
+                            {
+                                if (hotel[i][j] == "O")
+                                {
+                                    hotel[i][j] = "X";
+                                    Console.WriteLine($"Piso {i} | Puerta {j} RESERVADA!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Piso {i} | Puerta {j} ESTA OCUPADA!");
+                                }
+                            }
+                        }
+                    }
+                    System.Console.WriteLine();
+                    System.Console.WriteLine("Pulsa enter para continuar...");
+                    Console.ReadLine();
+                    break;
+                case 3:
+                    System.Console.WriteLine("Que reserva quieres borrar:");
+                    System.Console.Write("Piso: ");
+                    userFloor = Convert.ToInt32(Console.ReadLine());
+                    System.Console.Write("Puerta: ");
+                    userDoor = Convert.ToInt32(Console.ReadLine());
 
-        //             for (int i = 0; i < hotel.Length; i++)
-        //             {
-        //                 for (int j = 0; j < hotel[i].Length; j++)
-        //                 {
-        //                     if (i == userFloor && j == userDoor)
-        //                     {
-        //                         if (hotel[i][j] == "X")
-        //                         {
-        //                             hotel[i][j] = "O";
-        //                             Console.WriteLine($"RESERVA EN Piso {i} | Puerta {j} ! BORRADA!");
-        //                         }
-        //                         else
-        //                         {
-        //                             Console.WriteLine($"Piso {i} | Puerta {j} NO TIENE NINGUNA RESERVA!");
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //             System.Console.WriteLine("Quieres cambiar la reserva a otra habitacion? (S/N)");
-        //             string userModify = Console.ReadLine();
-        //             if (userModify != "N")
-        //             {
-        //                 goto case 2;
-        //             }
-        //             break;
-        //         case 4:
-        //             System.Console.WriteLine("Cerrando programa...");
-        //             break;
-        //         default: 
-        //             System.Console.WriteLine("No has introducido ningun numero valido!");
-        //             break;
-        //     }
-        //     System.Console.WriteLine();
-        // } while (userSelect != 4);
+                    for (int i = 0; i < hotel.Length; i++)
+                    {
+                        for (int j = 0; j < hotel[i].Length; j++)
+                        {
+                            if (i == userFloor && j == userDoor)
+                            {
+                                if (hotel[i][j] == "X")
+                                {
+                                    hotel[i][j] = "O";
+                                    Console.WriteLine($"RESERVA EN Piso {i} | Puerta {j} ! BORRADA!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Piso {i} | Puerta {j} NO TIENE NINGUNA RESERVA!");
+                                }
+                            }
+                        }
+                    }
+                    System.Console.WriteLine("Quieres cambiar la reserva a otra habitacion? (s/n)");
+                    string userModify = Console.ReadLine()!;
+                    if (userModify == "s")
+                    {
+                        goto case 2;
+                    }
+                    break;
+                case 4:
+                    System.Console.WriteLine("Cerrando programa...");
+                    break;
+                default: 
+                    System.Console.WriteLine("No has introducido ningun numero valido!");
+                    break;
+            }
+            System.Console.WriteLine();
+        } while (userSelect != 4);
+        System.Console.WriteLine();
         
     
         // 3. Una tienda de comestibles ha registrado las ventas de productos en diferentes
@@ -182,21 +184,22 @@
         // sección específica. Codifica un programa en C# que calcule el total de ventas y
         // muestre el total de ventas por sección (Cada línea es una sección).
 
-        // int totalVentas = 0;
+        int totalVentas = 0;
 
-        // for (int i = 0; i < ventas.GetLength(0); i++)
-        // {
-        //     int totalSec = 0;
+        for (int i = 0; i < ventas.GetLength(0); i++)
+        {
+            int totalSec = 0;
             
-        //     for (int j = 0; j < ventas.GetLength(1); j++)
-        //     {
-        //         totalSec += ventas[i,j];
-        //         totalVentas += ventas[i,j];
-        //     }
+            for (int j = 0; j < ventas.GetLength(1); j++)
+            {
+                totalSec += ventas[i,j];
+                totalVentas += ventas[i,j];
+            }
 
-        //     Console.WriteLine($"VENTAS SECCION {i}: {totalSec}");
-        // }
-        // Console.WriteLine($"TOTAL VENTAS: {totalVentas}");
+            Console.WriteLine($"VENTAS SECCION {i}: {totalSec}");
+        }
+        Console.WriteLine($"TOTAL VENTAS: {totalVentas}");
+        System.Console.WriteLine();
 
         // 4. En un colegio, se lleva un registro de la asistencia de los estudiantes en diferentes
         // clases a lo largo de la semana. La información se presenta en una matriz de la
@@ -215,55 +218,53 @@
         // estudiante y el día de la semana, y el programa debe mostrar si el estudiante asistió o
         // no a esa clase.
 
-        // int userNum, weekDay;
+        int userNum, weekDay;
 
-        // System.Console.Write("Numero del estudiante: ");
-        // userNum = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("Numero del estudiante: ");
+        userNum = Convert.ToInt32(Console.ReadLine());
         
-        // System.Console.Write("Numero del dia de la semana: ");
-        // weekDay = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("Numero del dia de la semana: ");
+        weekDay = Convert.ToInt32(Console.ReadLine());
 
-        // if (asistencia[userNum - 1, weekDay - 1])
-        // {
-        //     Console.WriteLine("El alumno asistio a clase");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("El alumno NO asistio a clase");
-        // }
+        if (asistencia[userNum - 1, weekDay - 1])
+        {
+            Console.WriteLine("El alumno asistio a clase");
+        }
+        else
+        {
+            Console.WriteLine("El alumno NO asistio a clase");
+        }
+        System.Console.WriteLine();
 
         // 5. Explica el funcionamiento de este programa:
 
-        int[][] matriz = { // Declara la variable matriz y agrega valores
+        int[][] matriz = {
             new int[] { 1, 1, 1 },
             new int[] { 1 },
             new int[] { 1, 1, 2 }
         };
-        for (int i = 0; i < 3; i++) // Bucle para recorrer las filas de la matriz
+
+        for (int i = 0; i < matriz.Length; i++) // bucle para recorrer cada fila
         {
-            int sumaFila = 0; // Guarda valores de las filas
-            int sumaColumna = 0; // Guardar valores de las columnas
-            for (int j = 0; j < matriz[i].Length; j++) // Bucle para recorrer los valores de la fila
+            int sumaFila = 0; // variable para guardar total de cada fila, se resetea cada vez que cambiamos a una nueva
+            for (int j = 0; j < matriz[i].Length; j++) // bucle para recorrer cada valor en la fila
             {
-                if (i < matriz[j].Length) // Si el indice de fila es menor a la longitud de la matriz con el indice de la columna
+                sumaFila += matriz[i][j]; // sumamos el valor dentro de las filas
+            }
+            Console.WriteLine($"Suma total fila {i+1}: {sumaFila}"); // mostramos el total de cada fila
+        }
+
+        for (int i = 0; i < matriz.Length; i++) // bucle para recorrer filas, pero lo tratamos como si recorriese columnas
+        {
+            int sumaColumna = 0; // variable para guardar total de cada columna, se resetea cada vez que cambiamos a una nueva
+            for (int j = 0; j < matriz.Length; j++) // bucle para recorrer cada valor en la columna
+            {
+                if (i < matriz[j].Length) // si la actual columna es mayor que la longitud de la linea, no hay valor en esa posicion
                 {
-                    sumaFila += matriz[i][j]; // Suma valor al sumaFila
-                }
-                if (j < matriz[i].Length && i < matriz[j].Length)
-                {
-                    sumaColumna += matriz[i][j]; // Suma valor al sumaColumna
+                    sumaColumna += matriz[j][i]; // sumamos el valor dentro de las columnas
                 }
             }
-            Console.WriteLine("Suma de fila: " + sumaFila);
-            Console.WriteLine("Suma de columna: " + sumaColumna); // Printa los valores
+            Console.WriteLine($"Suma total columna {i+1}: {sumaColumna}"); // mostramos el total de cada columna
         }
-        
-        // Un problema de este codigo es que si la fila tiene menor longitud a la columna, hay valores de la columna
-        // que se pierden porque el bucle no lo recorre.
-
-        // Otro problema es que al no cumplir la condicion "i < matriz[j].Length", no suma ni las filas ni columnas.
-
-        
-
     }
 }
